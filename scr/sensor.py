f75ad3834948d6dbcbec8847f08f848a490647ff
20136806 <20136806@tafe.wa.edu.au>
2024-11-19 08:31:14 -0300

    Fixing sensor class, miss typing

--- scr/sensor.py
+++ scr/sensor.py
@@ -1,7 +1,7 @@
 class Sensor:
     def __init__(self, id, is_active, car_park):
         self.id = id
-        self.is_active = is_activegit
+        self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
